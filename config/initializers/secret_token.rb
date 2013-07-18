afda8d0f70d8d0519ac0152f5be936a759aebd0b
Sandesh Sethi <nictesh@gmail.com>
2013-07-19 02:40:51 +0530

    Created app and add status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freebook::Application.config.secret_token = 'dcdfe7f51ccfd1c56ae0f28acbb62df2beba5416b4c361fec065a1eb7acbcec8f128d065dcc3eb0ad875c9cfd242d9f4b474ad48b64657abc6b7e71612afe692'
