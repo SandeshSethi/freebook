2102f9fc281b1adb62621c53c592f7ab8ce72e8f
Sandesh Sethi <nictesh@gmail.com>
2013-07-20 03:54:05 +0530

    Added styles and nav links

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+
+	
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -6,5 +8,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, 
+  					:first_name, :last_name, :profile_name
+
+  	has_many :statuses					
+
+  def full_name	
+  	first_name + " " + last_name
+  end
+
 end
